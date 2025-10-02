627c2c28ee3a92358c128fd479b50e6fa0c48686
Dchrome <duy.vd2416686@sis.hust.edu.vn>
2025-10-02 13:41:49 +0700

    Commit lab01

--- JavaBasics/src/basics/ChoosingOption.java
+++
@@ -1,8 +0,0 @@
-package basics;
-import javax.swing.JOptionPane;
-public class ChoosingOption{
-	public static void main(String[] args){
-		JOptionPane.showMessageDialog(null,"Hello world! How are you?");
-		System.exit(0);
-	}
-}
